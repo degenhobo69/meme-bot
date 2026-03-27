6bb2a248875e83123c38b81616656e8659b935f8
degenhobo69 <degenhobo69@gmail.com>
2026-03-27 02:13:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import time
+import requests
 from telegram import Bot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -7,17 +8,39 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 bot = Bot(token=BOT_TOKEN)
 
+def get_memes():
+    url = "https://meme-api.com/gimme/5"
+
+    try:
+        res = requests.get(url, timeout=10)
+        data = res.json()
+
+        memes = []
+        for post in data["memes"]:
+            title = post["title"]
+            link = post["url"]
+            memes.append(f"{title}\n{link}")
+
+        return memes
+
+    except Exception as e:
+        print("Error:", e)
+        return []
+
 def run():
     while True:
-        try:
-            bot.send_message(
-                chat_id=CHAT_ID,
-                text="🔥 Bot is alive and working!"
-            )
-            time.sleep(60)  # every 1 minute
+        memes = get_memes()
 
-        except Exception as e:
-            print("Error:", e)
-            time.sleep(10)
+        for meme in memes:
+            try:
+                bot.send_message(
+                    chat_id=CHAT_ID,
+                    text=f"😂 {meme}"
+                )
+                time.sleep(3)
+            except Exception as e:
+                print("Send error:", e)
+
+        time.sleep(1800)  # every 30 minutes
 
 run()
